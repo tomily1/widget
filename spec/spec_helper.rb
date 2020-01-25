d8e9e9283c100ba7ed4a9d4056a3c242025ca46f
Israel Tomilayo <itomilayo@gmail.com>
2020-01-25 18:12:45 +0100

    add dotenv gem to manage environment variables

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,7 +47,7 @@ RSpec.configure do |config|
   # Allows RSpec to persist some state between runs in order to support
   # the `--only-failures` and `--next-failure` CLI options. We recommend
   # you configure your source control system to ignore this file.
-  config.example_status_persistence_file_path = "spec/.rspec_temp_data"
+  # config.example_status_persistence_file_path = "spec/.rspec_temp_data"
   config.disable_monkey_patching!
   #config.warnings = true
 
