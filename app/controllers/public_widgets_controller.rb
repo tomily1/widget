4c06cf542bd81d85a526619f912dbea914afa862
Isreal <itomilayo@gmail.com>
2020-01-30 19:14:26 +0100

    add kaminari pagination

--- app/controllers/public_widgets_controller.rb
+++ app/controllers/public_widgets_controller.rb
@@ -4,5 +4,6 @@ class PublicWidgetsController < ApplicationController
   def index
     result = ShowoffApi::Widget.new.fetch(token, params[:term] || '', true)
     @visible_widgets = result.code == :success ? result.data['widgets'] : []
+    @visible_widgets = Kaminari.paginate_array(@visible_widgets).page(params[:page]).per(9)
   end
 end
